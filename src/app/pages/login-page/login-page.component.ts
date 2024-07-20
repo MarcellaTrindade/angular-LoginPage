531917845fafd959aef542383ae595083f34255b
MarcellaTrindade <tchella12trindade@gmail.com>
2024-07-20 14:58:25 -0300

    update code

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,20 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { LoginService } from '../../services/login.service';
-import { ToastrService } from 'ngx-toastr';
-import { FormControl, FormGroup, FormRecord, ReactiveFormsModule, Validators } from '@angular/forms';
 
-interface LoginForm {
-  email: FormControl,
-  password: FormControl
-}
-
-interface SignupForm {
-  name: FormControl,
-  email: FormControl,
-  password: FormControl,
-  passwordConfirm: FormControl
-}
 
 @Component({
   selector: 'app-login-page',
@@ -22,22 +7,9 @@ interface SignupForm {
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent{
-    signupForm!: FormGroup<SignupForm>;
-    loginForm!: FormGroup<LoginForm>;
 
- constructor( private router: Router, private loginService: LoginService, private toastrService: ToastrService){
-    this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
-    })
-
-    this.signupForm = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    })
- }
+ constructor(){
+    }
 
 
   showFormu: boolean = true;
@@ -77,18 +49,5 @@ export class LoginPageComponent{
       'border-radius': '100px 25px 25px 100px',
       'transition': 'all 0.6s ease-in-out',
     }
-  }
-  submit(){
-    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-      next: () => this.toastrService.success("Login feito com sucesso!"),
-      error: () => this.toastrService.error("Erro inesperado! Tente novamente mais tarde")
-    })
-  }
-
-  signup(){
-    this.loginService.signup(this.signupForm.value.name, this.signupForm.value.email, this.signupForm.value.password).subscribe({
-      next: () => this.toastrService.success("Login feito com sucesso!"),
-      error: () => this.toastrService.error("Erro inesperado! Tente novamente mais tarde")
-    })
   }
 }
